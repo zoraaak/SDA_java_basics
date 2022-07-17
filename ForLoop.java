b80508a595386fe2b2ccb937a30f2693ef5a4713
Tomasz Jędrzejczyk <M@rtaB@rto1989>
2022-07-17 20:18:09 +0200

    init new repo for basics of java

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        String[] dogs = new String[]{"Rex", "Scooby", "Szarik", "Sara"};
+
+        for (String dog : dogs) {
+            System.out.println(dog);
+        }
+    }
+}
---
+++ ForiLoop.java
@@ -0,0 +1,9 @@
+public class ForiLoop {
+    public static void main(String[] args) {
+        String[] dogs = new String[]{"Rex", "Scooby", "Szarik", "Sara"};
+
+        for (int i = 0; i < dogs.length; i++) {
+            System.out.println(dogs[i]);
+        }
+    }
+}
