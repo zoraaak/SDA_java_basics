7af8c314d84d0f2614f76598043c50bb02aa3f6a
zoraaak <116521983+zoraaak@users.noreply.github.com>
2022-10-26 19:58:40 +0200

    Krolik1

--- src/Main1.java
+++ src/Main1.java
@@ -2,6 +2,9 @@ public class Main1 {
     public static void main(String[] args) {
 
     Rabbit krolik1=new Rabbit(50, "Hanek", true);
+    krolik1.changeName("Stanislaw");
+
+
 
 
 
---
+++ src/Rabbit.java
@@ -0,0 +1,20 @@
+public class Rabbit {
+    private int length;
+    private String name;
+    private boolean softness;
+
+
+    Rabbit(int length, String name, boolean softness){
+        this.length= length;
+        this.name= name;
+        this. softness= softness;
+    }
+    public void changeName(String name){
+        this.name= name;
+
+    }
+
+
+
+
+}
